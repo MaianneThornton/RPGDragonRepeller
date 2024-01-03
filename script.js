b059ded0a7b02d563250d86b53d1a292d52f7947
Maianne Thornton <maianne.thornton@gmail.com>
2024-01-03 11:55:02 -0500

    step 33, created button2 & button 3 vars

--- script.js
+++ script.js
@@ -6,3 +6,5 @@ let fighting;
 let monsterHealth;
 let inventory = ['stick'];
 const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
