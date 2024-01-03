72fa74e58b4e1ef928fbd756fb4c31a2bde37d29
Maianne Thornton <maianne.thornton@gmail.com>
2024-01-03 12:51:04 -0500

    refactor goStore function

--- script.js
+++ script.js
@@ -23,10 +23,10 @@ button3.onclick = fightDragon;
 
 function goStore() {
     button1.innerText = "Buy 10 health (10 gold)";
-    button1.onclick = buyHealth;
     button2.innerText = "Buy weapon (30 gold)";
-    button2.onclick = buyWeapon;
     button3.innerText = "Go to town square";
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
     button3.onclick = goTown;
 }
 function goCave() {
