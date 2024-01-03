58970ae5700eb586f81a37b7ea475325a73c6118
Maianne Thornton <maianne.thornton@gmail.com>
2024-01-03 13:18:25 -0500

    step 58, text property to locations var object

--- script.js
+++ script.js
@@ -19,7 +19,8 @@ const locations = [
     {
         name: "town square",
         "button text": ["Go to store", "Go to cave", "Fight dragon"],
-        "button functions": [goStore, goCave, fightDragon]
+        "button functions": [goStore, goCave, fightDragon],
+        text: "You are in the town square. You see a sign that says \"Store\"."
     }
 ];
 
