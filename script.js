e1de928a0ca4157ed3793d9873985b75ac156045
Maianne Thornton <maianne.thornton@gmail.com>
2024-01-03 13:36:11 -0500

    step 66, changed value of button2 & button3 innerText to use bracket notation to get second and third button texts in update function respectively

--- script.js
+++ script.js
@@ -37,8 +37,8 @@ button3.onclick = fightDragon;
 
 function update(location) {
     button1.innerText = location["button text"][0];
-    button2.innerText = "Go to cave";
-    button3.innerText = "Fight dragon";
+    button2.innerText = location["button text"][1];
+    button3.innerText = location["button text"][2];
     button1.onclick = goStore;
     button2.onclick = goCave;
     button3.onclick = fightDragon;
