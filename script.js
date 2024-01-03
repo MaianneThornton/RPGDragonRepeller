31d55e620450c85de53d4a7f301d38ac0d777c98
Maianne Thornton <maianne.thornton@gmail.com>
2024-01-03 12:07:58 -0500

    step 36, added intialize buttons comment

--- script.js
+++ script.js
@@ -15,3 +15,4 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+//initialize buttons
