eaaece618e47a619078f96621f533d394310c943
Maianne Thornton <maianne.thornton@gmail.com>
2024-01-05 11:11:50 -0500

    step 100, button2 = sellWeapon

--- script.js
+++ script.js
@@ -114,10 +114,12 @@ function buyWeapon() {
       inventory.push(newWeapon);
       text.innerText += " In your inventory you have: " + inventory;
     } else {
-      text.innerText = "You do not have enough gold to buy a weapon."
+      text.innerText = "You do not have enough gold to buy a weapon.";
     }
   } else {
-    text.innerText = "You already have the most powerful weapon!"
+    text.innerText = "You already have the most powerful weapon!";
+    button2.innerText = "Sell weapon for 15 gold";
+    button2.onclick = sellWeapon;
   }
 }
 
