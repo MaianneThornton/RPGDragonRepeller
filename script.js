fa55d9c1474187dc80a21e722b91079d9b27c54b
Maianne Thornton <maianne.thornton@gmail.com>
2024-01-05 10:52:08 -0500

    step 94, added contents of inventory to text

--- script.js
+++ script.js
@@ -111,6 +111,7 @@ function buyWeapon() {
     let newWeapon = weapons[currentWeapon].name;
     text.innerText = 'You now have a ' + newWeapon + ".";
     inventory.push(newWeapon);
+    text.innerText += " In your inventory you have: " + inventory;
   }
 }
 
