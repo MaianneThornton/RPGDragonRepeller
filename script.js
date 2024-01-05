bbc723a5f650c3119731edc3b0bbbaa2853b8c8d
Maianne Thornton <maianne.thornton@gmail.com>
2024-01-05 12:24:42 -0500

    step 118: current monster fighting health update and display

--- script.js
+++ script.js
@@ -154,7 +154,13 @@ function sellWeapon() {
   }
 }
 
-function goFight() { };
+function goFight() {
+  update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = "block";
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsters[fighting].health;
+ };
 
 function fightSlime() {
   fighting = 0;
