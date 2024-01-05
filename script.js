b87874f87b511410e42dfc767147d607b743d0e9
Maianne Thornton <maianne.thornton@gmail.com>
2024-01-05 11:50:01 -0500

    step 109, monsters var complete

--- script.js
+++ script.js
@@ -33,6 +33,23 @@ const weapons = [
     power: 100,
   }
 ];
+const monsters = [
+  {
+    name: "slime",
+    level: 2,
+    health: 15,
+  },
+  {
+    name: "fanged beast",
+    level: 8,
+    health: 60,
+  },
+  {
+    name: "dragon",
+    level: 20,
+    health: 300,
+  },
+];
 const locations = [
   {
     name: 'town square',
