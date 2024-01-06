2533ffd1a547e9c3544fa88b1a867a89056bc17b
Maianne Thornton <maianne.thornton@gmail.com>
2024-01-06 15:46:27 -0500

    step 142: added win obj to locations array

--- script.js
+++ script.js
@@ -90,6 +90,12 @@ const locations = [
     'button text': ["REPLAY?", "REPLAY?", "REPLAY?"],
     'button functions': [restart, restart, restart],
     text: "You die. ☠️"
+  },
+  {
+    name: "win",
+    'button text': ["REPLAY?", "REPLAY?", "REPLAY?"],
+    'button functions': [restart, restart, restart],
+    text: "You defeat the dragon! YOU WIN THE GAME! 🎉"
   }
 ];
 
